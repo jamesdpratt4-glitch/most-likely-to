c5754122792a6d79894531eed5e624c06d2ab8a4
James Pratt <jamesdpratt4@gmail.com>
2026-05-16 13:36:12 +0100

    Improve question text readability with larger font and better spacing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import GameOver from './pages/GameOver'
 import JoinRoom from './pages/JoinRoom'
 import './App.css'
 
-const APP_VERSION = "1.0.55"
+const APP_VERSION = "1.0.56"
 
 function Home() {
   const navigate = useNavigate()
