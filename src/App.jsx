05351284b5872da0078cf06e4df1ef8fef5c1a71
James Pratt <jamesdpratt4@gmail.com>
2026-05-16 11:29:46 +0100

    Bump version to 1.0.18

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import PlayerLobby from './pages/PlayerLobby'
 import Game from './pages/Game'
 import './App.css'
 
-const APP_VERSION = "1.0.17"
+const APP_VERSION = "1.0.18"
 
 function Home() {
   const navigate = useNavigate()
